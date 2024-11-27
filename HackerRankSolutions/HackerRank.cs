9a0e1b5eed514d7e6d65b784ff8531199edb7691
Teejay2443 <tijaniabdulafeez05@gmail.com>
2024-11-27 15:55:18 +0100

    Add project files.

---
+++ HackerRankSolutions/HackerRank.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankSolutions
+{
+    public static class HackerRank
+    {
+        public static int sum(int a,int b)
+        {
+            int sum = a + b;
+			return sum;
+        }
+        public static int simpleArraySum(List<int> array)
+        {
+            int i = 0;
+            foreach (int sums in array)
+            {
+                i += sums;
+            }
+            return i;
+        }
+        public static long aVeryBigSum(List<long> ar)
+        {
+            long sum = 0;
+            foreach (var num in ar)
+            {
+                sum += num;
+            }
+            return sum;
+        }
+        public static int diagonalDifference(List<List<int>> arr)
+        {
+
+            int n = arr.Count;
+            int Sum = 0;
+            int Sum2 = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                Sum += arr[i][i];
+                Sum2 += arr[i][n - i - 1];
+            }
+
+            return Math.Abs(Sum2 - Sum);
+        }
+        public static void plusMinus(List<int> arr)
+        {
+            int n = arr.Count;
+            int positive = 0;
+            int negative = 0;
+            int zero = 0;
+            foreach (int number in arr)
+            {
+                if (number > 0)
+                {
+                    positive++;
+
+                }
+                else if (number < 0)
+                {
+                    negative++;
+                }
+                else
+                {
+                    zero++;
+                }
+            }
+            double hhh = (double)positive / n;
+            double hh = (double)negative / n;
+            double h = (double)zero / n;
+            Console.WriteLine(hhh);
+            Console.WriteLine(hh);
+            Console.WriteLine(h);
+        }
+		public static void miniMaxSum(List<int> arr)
+		{
+
+			arr.Sort();
+			long minSum = (long)arr[0] + arr[1] + arr[2] + arr[3];
+			long maxSum = (long)arr[1] + arr[2] + arr[3] + arr[4];
+			Console.WriteLine(minSum + " " + maxSum);
+		}
+		public static int birthdayCakeCandles(List<int> candles)
+		{
+			var num = candles.Max();
+			int count = candles.Count(x => x == num);
+			return count;
+		}
+	}
+    public static class codechallenge
+    {
+        public static int basketBall(string[] input)
+        {
+			try
+            {
+				List<int> points = new List<int>();
+
+                foreach (var point in input)
+                {
+
+                    if (point == "+")
+                    {
+                        int sum = points[points.Count - 1] + points[points.Count - 2];
+                        points.Add(sum);
+                    }
+                    else if (point == "D")
+                    {
+                        int Dpoint = 2 * points[points.Count - 1];
+                        points.Add(Dpoint);
+                    }
+                    else if (point == "C")
+                    {
+                        int Cpoint = points[points.Count - 1];
+                        points.Remove(Cpoint);
+                    }
+                    else
+                    {
+                        points.Add(Convert.ToInt32(point));
+                    }
+                }
+                int Sumtoatal = 0;
+                foreach (var score in points)
+                {
+                    Sumtoatal += score;
+                }
+                return Sumtoatal;
+            }
+            catch(Exception e) 
+            {
+                Console.WriteLine(e.Message);
+            }
+            return 0;
+
+        }
+    }  
+
+}
---
+++ HackerRankSolutions/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using HackerRankSolutions;
+using System.Data;
+using System.Diagnostics.CodeAnalysis;
+
+//Console.WriteLine(HackerRank.sum(2,5));
+
+//Basket 
+
+using System;
+using System.Collections.Generic;
+
+string[] input = { "5", "2", "C", "D", "+" };
+Console.WriteLine("Your final point for the basketball team = " + codechallenge.basketBall(input));
+
+
+
+
+
